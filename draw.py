8d510d9ca4950cfd469a95e2e9f0e59dae96e7da
KimSergey <kims230599@gmail.com>
2015-10-14 21:14:07 +0300

    Beta

--- draw.py
+++ draw.py
@@ -5,8 +5,19 @@ import sys
 import time
 
 X1, Y1, X2, Y2, X3, Y3, X4, Y4 = 4, 0, 4, 8, 0, 4, 8, 4
+step_first = 1
+step_second = 1
 def button_clicked(self):
-    bg = '#%0x%0x%0x' % (8, 1, 8)
+    global step_first
+    global step_second
+    if step_first == step_second:
+        bg = '#%0x%0x%0x' % (1, 8, 1)
+        self.widget['text'] = step_first
+        step_first += 1
+    else:
+        bg = '#%0x%0x%0x' % (8, 8, 1)
+        self.widget['text'] = step_second
+        step_second += 1
     self.widget['bg'] = bg
     self.widget['activebackground'] = bg
 
@@ -22,19 +33,28 @@ can.create_rectangle(450-1, 90-1, 1110-1, 750-1, fill="#AA0A0A")
 for i in range(9):
     for j in range(9):
         if i == X1 and j == Y1 or i == X2 and j == Y2 or i == X3 and j == Y3 or i == X4 and j == Y4:
-            places_squares[i][j] = Button(root, bg='#804020')
+            places_squares[i][j] = Button(root, bg='#804020', text='0')
             places_squares[i][j].place(x=450+75*i, y=90+75*j, width=60, height=60)
         else:
             places_squares[i][j] = Button(root, bg='#A00A0A')
-            places_squares[i][j].place(x=450+75*i, y=90+75*j, width=60, height=60)
+            places_squares[i][j].place(x=450+75*i, 
+                                       y=90+75*j, 
+                                       width=60, 
+                                       height=60) # Мотяга фарива!1!!
 for i in range(9):
     for j in range(8):
         spaces_horizontal[i][j] = Button(root, bg='#800808')
-        spaces_horizontal[i][j].place(x=450+75*i, y=150+75*j, width=60, height=15)
+        spaces_horizontal[i][j].place(x=450+75*i, 
+                                      y=150+75*j, 
+                                      width=60, 
+                                      height=15)
 for i in range(8):
     for j in range(9):
         spaces_vertical[i][j] = Button(root, bg='#800808')
-        spaces_vertical[i][j].place(x=510+75*i, y=90+75*j, width=15, height=60)
+        spaces_vertical[i][j].place(x=510+75*i, 
+                                    y=90+75*j, 
+                                    width=15, 
+                                    height=60)
 can.pack()
 root.bind_class('Button', '<1>', button_clicked)
 root.mainloop()
