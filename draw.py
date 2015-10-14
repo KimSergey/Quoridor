69edbd8629148ef85bc3fac3c75a689a601198c7
KimSergey <kims230599@gmail.com>
2015-10-14 16:33:20 +0300

    For nobel players. We cant to go into our parents

--- draw.py
+++ draw.py
@@ -1,73 +1,40 @@
-X1, Y1, X2, Y2 = 4, 0, 4, 8
-
+#coding:utf-8
+# -*- coding: utf-8 -*-
 from tkinter import *
 import sys
-import time 
+import time
 
-class Figure:
-    
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def move(self, direct):
-        if direct == 1:
-            self.x += 1
-        if direct == 2:
-            self.y += 1
-        if direct == 3:
-            self.x -= 1
-        if direct == 4:
-            self.y -= 1
-            
+X1, Y1, X2, Y2, X3, Y3, X4, Y4 = 4, 0, 4, 8, 0, 4, 8, 4
+def button_clicked(self):
+    bg = '#%0x%0x%0x' % (8, 1, 8)
+    self.widget['bg'] = bg
+    self.widget['activebackground'] = bg
 
-
-squares = []
-def create_table(x1, y1, x2, y2):
-    
-    root = Tk()
-    root.title("Game")
-    root.wm_geometry("+%d+%d" % (0, 0))
-    can = Canvas(root, width = root.winfo_screenwidth() - 18, height = root.winfo_screenheight())
-    can.create_rectangle(480, 47, 1120, 677, fill = "black")
-    can.create_rectangle(485, 52, 1115, 672, fill = "brown")
-
-    for i in range(9):
-        for j in range(9):
-            if i == x1 and j == y1:
-                but = Button(root, bg = 'red')
-            elif i == x2 and j == y2:
-                but = Button(root, bg = 'yellow')
-            else:
-                but = Button(root, bg = 'orange')
-            but.place(relx = 0.3135 + i / 23, rely = 0.688 - j / 13, width = 55, height = 55)
-            squares.append(but)
-                
-    
-    for i in range(9):
-        for j in range(8):
-            but = Button(root, bg='brown')
-            but.place(x=497 + 69 * i, y=118 + 67.5 * j, width=54, height=12)
-    
-    for i in range(8):
-        for j in range(9):
-            but = Button(root, bg='brown')
-            but.place(x=552 + 69 * i, y=64 + 67.5 * j, width=13, height=54)
-    can.pack()
-    root.mainloop()
-create_table(X1, Y1, X2, Y2)
-
-
-
-
-figure_white = Figure(4, 0)
-figure_black = Figure(4, 8)
-step_number = 1
-while True:
-    if step_number % 2 == 1:
-        for but in squares:
-            but.bind('<Button-1>', create_table(1, 1, 3, 3))
-    if step_number % 2 == 0:
-        for but in squares:
-            but.bind('<Button-1>', create_table(2, 2, 4, 4))
-    step_number += 1
+places_squares = [[None for i in range(9)] for j in range(9)]
+spaces_horizontal = [[None for i in range(8)] for j in range(9)]
+spaces_vertical = [[None for i in range(9)] for j in range(8)]
+root = Tk()
+root.title("Game")
+root.wm_geometry("+%d+%d" % (0, 0))
+can = Canvas(root, width = root.winfo_screenwidth() - 18, height = root.winfo_screenheight())
+can.create_rectangle(445-1, 85-1, 1115-1, 755-1, fill="black") #one pixel is very important
+can.create_rectangle(450-1, 90-1, 1110-1, 750-1, fill="#AA0A0A")
+for i in range(9):
+    for j in range(9):
+        if i == X1 and j == Y1 or i == X2 and j == Y2 or i == X3 and j == Y3 or i == X4 and j == Y4:
+            places_squares[i][j] = Button(root, bg='#804020')
+            places_squares[i][j].place(x=450+75*i, y=90+75*j, width=60, height=60)
+        else:
+            places_squares[i][j] = Button(root, bg='#A00A0A')
+            places_squares[i][j].place(x=450+75*i, y=90+75*j, width=60, height=60)
+for i in range(9):
+    for j in range(8):
+        spaces_horizontal[i][j] = Button(root, bg='#800808')
+        spaces_horizontal[i][j].place(x=450+75*i, y=150+75*j, width=60, height=15)
+for i in range(8):
+    for j in range(9):
+        spaces_vertical[i][j] = Button(root, bg='#800808')
+        spaces_vertical[i][j].place(x=510+75*i, y=90+75*j, width=15, height=60)
+can.pack()
+root.bind_class('Button', '<1>', button_clicked)
+root.mainloop()
