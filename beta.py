2abc028596da41189d3f248e632cbb83a70ea9a6
KimSergey <kims230599@gmail.com>
2015-10-15 19:17:54 +0300

    Update beta.py

--- beta.py
+++ beta.py
@@ -3,7 +3,7 @@
 
 from tkinter import *
 
-X1, Y1, X2, Y2, X3, Y3, X4, Y4 = 4, 0, 4, 8, 0, 4, 8, 4
+X1, Y1, X2, Y2 = 4, 0, 4, 8
 step_first = 1
 step_second = 1
 information_first = 'P40'
@@ -20,9 +20,9 @@ def button_clicked(self):
     information_self = self.widget['text'][0:3]
     if step_first == step_second:
         if information_self[0] == 'F':
+            step_first += 1
             self.widget['text'] = str(step_first)
             self.widget['bg'] = '#%0x%0x%0x' % (1, 3, 1)
-            step_first += 1
             return
         bg = '#%0x%0x%0x' % (1, 8, 1)
         if (abs(int(information_first[1]) - int(information_self[1])) > 1 or
@@ -37,9 +37,9 @@ def button_clicked(self):
         step_first += 1
     else:
         if information_self[0] == 'F':
+            step_second += 1
             self.widget['text'] = str(step_second)
             self.widget['bg'] = '#%0x%0x%0x' % (1, 3, 1)
-            step_second += 1
             return
         bg = '#%0x%0x%0x' % (8, 8, 1)
         if (abs(int(information_second[1]) - int(information_self[1])) > 1 or
@@ -63,19 +63,16 @@ can.create_rectangle(445-1, 85-1, 1115-1, 755-1, fill="black") #one pixel is ver
 can.create_rectangle(450-1, 90-1, 1110-1, 750-1, fill="#AA0A0A")
 for i in range(9):
     for j in range(9):
-        if i == X1 and j == Y1 or i == X2 and j == Y2 or i == X3 and j == Y3 or i == X4 and j == Y4:
-            places_squares[i][j] = Button(root, bg='#804020', text='P'+str(i)+str(j))
-            places_squares[i][j].place(x=450+75*i, 
-                                       y=90+75*j, 
-                                       width=60, 
-                                       height=60)
-
-        else:
-            places_squares[i][j] = Button(root, bg='#A00A0A', text='P'+str(i)+str(j))
-            places_squares[i][j].place(x=450+75*i, 
-                                       y=90+75*j, 
-                                       width=60, 
-                                       height=60)
+        colour = '#A00A0A'
+        if i == X1 and j == Y1:
+            colour = '#%0x%0x%0x' % (1, 8, 1)
+        if i == X2 and j == Y2:
+            colour = '#%0x%0x%0x' % (8, 8, 1)
+        places_squares[i][j] = Button(root, bg=colour, text='P'+str(i)+str(j))
+        places_squares[i][j].place(x=450+75*i, 
+                                   y=90+75*j, 
+                                   width=60, 
+                                   height=60)
 for i in range(9):
     for j in range(8):
         spaces_horizontal[i][j] = Button(root, bg='#800808', text='F'+'  ')
